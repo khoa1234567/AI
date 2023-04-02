354273fc61b7c5c778059d87bf0eeda07828d7d6
khoa1234567 <khoabg99@gmail.com>
2023-04-03 02:40:30 +0700

    Update chupanh.py

--- chupanh.py
+++ chupanh.py
@@ -1,11 +1,10 @@
-import cv2
+import cv2, math
 import mediapipe as mp
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import threading
 import os
-import pickle
 
 window = tk.Tk()
 window.title("Hand Tracking")
@@ -50,15 +49,17 @@ def update_image_label():
 # --------------------- Phần lưu file dữ liệu nhận diện bàn tay ---------------------
 def save_hand_landmarks():
     img = capture_and_process()
-    save_path = "hello.xml"
-
+    save_path = filedialog.asksaveasfilename(defaultextension='.xml', filetypes=[('XML files', '.xml'), ('All files', '.*')])
     if os.path.exists(save_path):
         with open(save_path, "a") as f:
             anh_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             ket_qua = tay.process(anh_rgb)
             if ket_qua.multi_hand_landmarks:
                 for hand_landmarks in ket_qua.multi_hand_landmarks:
-                    f.write(str(hand_landmarks))
+                    landmarks = []
+                    for landmark in hand_landmarks.landmark:
+                        landmarks.extend([landmark.x, landmark.y, landmark.z])
+                    f.write(' '.join(map(str, landmarks)))
                     f.write("\n")
     else:
         with open(save_path, "w") as f:
@@ -66,39 +67,64 @@ def save_hand_landmarks():
             ket_qua = tay.process(anh_rgb)
             if ket_qua.multi_hand_landmarks:
                 for hand_landmarks in ket_qua.multi_hand_landmarks:
-                    f.write(str(hand_landmarks))
+                    landmarks = []
+                    for landmark in hand_landmarks.landmark:
+                        landmarks.extend([landmark.x, landmark.y, landmark.z])
+                    f.write(' '.join(map(str, landmarks)))
                     f.write("\n")
 
     print(f"Đã lưu dữ liệu vào file: {save_path}")
 
-
 # --------------------- Phần đọc file dữ liệu và hiển thị lên màn hình ---------------------
-def read_and_display_data():
+
+def display_data():
+    # Đọc file XML
     file_path = filedialog.askopenfilename(filetypes=[("XML files", "*.xml")])
     if not file_path:
         return
     with open(file_path, "r") as f:
         data = f.read()
-    hand_landmarks = []
+
+    # Tạo danh sách các điểm tọa độ trong file XML
+    xml_landmarks = []
     for line in data.splitlines():
         if line.startswith("x") or line.startswith("y") or line.startswith("z"):
             continue
         landmark = line.strip().split(" ")
         landmark = [float(val) for val in landmark]
-        hand_landmarks.append(landmark)
+        xml_landmarks.append(landmark)
 
-    # Hiển thị tên file XML lên màn hình
-    file_name_label.config(text=f"File: {os.path.basename(file_path)}")
-
-    # Vẽ khoảng vùng bàn tay trên camera
     while True:
         ret, khung_anh = cap.read()
         if not ret:
             break
         khung_anh = cv2.flip(khung_anh, 1)
-        for landmark in hand_landmarks:
-            x, y = int(landmark[1] * khung_anh.shape[1]), int(landmark[2] * khung_anh.shape[0])
-            cv2.circle(khung_anh, (x, y), 5, (0, 255, 0), -1)
+
+        # Tìm các điểm đặc trưng trên tay
+        anh_rgb = cv2.cvtColor(khung_anh, cv2.COLOR_BGR2RGB)
+        ket_qua = tay.process(anh_rgb)
+
+        if ket_qua.multi_hand_landmarks:
+            for hand_landmarks in ket_qua.multi_hand_landmarks:
+                landmarks = []
+                for landmark in hand_landmarks.landmark:
+                    landmarks.extend([landmark.x, landmark.y, landmark.z])
+
+                # So sánh tọa độ các điểm tay với tọa độ các điểm trong file XML
+                matched_points = 0
+                for xml_point in xml_landmarks:
+                    for point in landmarks:
+                        if isinstance(point, list) and all(abs(point[i] - xml_point[i]) < 0.05 for i in range(3)):
+                            matched_points += 1
+
+                # Nếu có ít nhất 80% các điểm khớp với tọa độ trong file XML thì hiển thị tên file lên màn hình
+                if matched_points >= len(xml_landmarks) * 0.8:
+                    file_name_label.config(text=f"File: {os.path.basename(file_path)}")
+                else:
+                    file_name_label.config(text="")
+
+                # Vẽ các điểm đặc trưng trên tay lên camera
+                mp_ghi_chu.draw_landmarks(khung_anh, hand_landmarks, mp_duyet_tay.HAND_CONNECTIONS)
 
         img = cv2.cvtColor(khung_anh, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(img)
@@ -111,57 +137,9 @@ def read_and_display_data():
 
     cap.release()
     cv2.destroyAllWindows()
-    # Tạo button để chọn file XML và hiển thị dữ liệu trên camera
-select_file_button = tk.Button(window, text="Chọn file XML", command=read_and_display_data)
-select_file_button.pack()
 
+# --------------------- Tạo giao diện ---------------------
 
-# --------------------- Phần lưu file dữ liệu nhận diện bàn tay ---------------------
-def read_and_display_data():
-    if not data_files:
-        return
-
-    file_path = filedialog.askopenfilename(filetypes=[("XML files", "*.xml")])
-    if file_path:
-        # Chạy phần đọc file dữ liệu trong một thread mới
-        threading.Thread(target=read_data_file, args=(file_path,), daemon=True).start()
-
-# Hàm đọc dữ liệu từ file và hiển thị trên camera
-def read_data_file(file_path):
-    with open(file_path, "r") as f:
-        data = f.read()
-    hand_landmarks = []
-    for line in data.splitlines():
-        if line.startswith("x") or line.startswith("y") or line.startswith("z"):
-            continue
-        landmark = line.strip().split(" ")
-        landmark = [float(val) for val in landmark]
-        hand_landmarks.append(landmark)
-
-    # Hiển thị tên file XML lên màn hình
-    file_name_label.config(text=f"File: {os.path.basename(file_path)}")
-
-    # Vẽ khoảng vùng bàn tay trên camera
-    while True:
-        ret, khung_anh = cap.read()
-        if not ret:
-            break
-        khung_anh = cv2.flip(khung_anh, 1)
-        for landmark in hand_landmarks:
-            x, y = int(landmark[1] * khung_anh.shape[1]), int(landmark[2] * khung_anh.shape[0])
-            cv2.circle(khung_anh, (x, y), 5, (0, 255, 0), -1)
-
-        img = cv2.cvtColor(khung_anh, cv2.COLOR_BGR2RGB)
-        img = Image.fromarray(img)
-        img = ImageTk.PhotoImage(img)
-
-        image_label.config(image=img)
-        image_label.image = img
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
 # --------------------- Tạo giao diện ---------------------
 
 # Tạo label để hiển thị ảnh từ camera
@@ -177,11 +155,15 @@ file_name_label = tk.Label(window, text="")
 file_name_label.pack()
 
 # Tạo button để đọc dữ liệu từ file đã lưu
-read_data_button = tk.Button(window, text="Đọc dữ liệu", command=read_and_display_data)
+read_data_button = tk.Button(window, text="Đọc dữ liệu", command=display_data)
 read_data_button.pack()
+
+# Tạo button để thoát chương trình
+exit_button = tk.Button(window, text="Thoát", command=window.destroy)
+exit_button.pack()
 
 # Khởi chạy phần update hình ảnh từ camera
 window.after(10, update_image_label)
+
 window.mainloop()
 
-cap.release()
